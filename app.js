560395dbfa255ce8b928b733a79e2c71c92718b1
The-Shinigami <mohamedbarbarr@gmail.com>
2022-02-14 13:36:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+/* var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync( 'index.html'); */
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+
+const parser = new parsers.Readline({
+     delimiter: '\r\n'
+});
+
+var port = new SerialPort('COM5',{ 
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+port.pipe(parser);
+
+var app = http.createServer(function(req, res){
+  
+    if(req.url === "/"){
+        fs.readFile("./public/index.html", "UTF-8", function(err, html){
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(html);
+        });
+    }else if(req.url.match("\.jpg$")){
+        var imagePath = path.join(__dirname, 'public', req.url);
+        var fileStream = fs.createReadStream(imagePath);
+        res.writeHead(200, { "Content-Type": "image/png" });
+        console.log(imagePath);
+        alert("hhhh")
+        fileStream.pipe(res);
+    }else{
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end("No Page Found");
+    }
+
+});
+
+var io = require('socket.io').listen(app);
+
+io.on('connection', function(socket) {
+    
+    console.log('Node is listening to port');
+    
+});
+
+
+
+
+parser.on('data', function(data) {
+    
+    console.log('Received data from port: ' + data);
+    io.emit('data', data);
+    
+});
+
+app.listen(3000,'0.0.0.0');
+
